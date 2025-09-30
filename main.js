f22595f2093ed4c25e3765611e4b3a9769414582
Arzoo Haroon <haroonarzoo77@gmail.com>
2025-09-30 15:10:31 +0100

    start

---
+++ main.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('enterBtn').addEventListener('click', () => {
+  document.body.style.overflow = 'auto';
+  document.querySelector('.hero').style.display = 'none';
+
+});
